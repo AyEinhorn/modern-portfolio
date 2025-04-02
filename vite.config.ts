02f06346cb648ea393b9800151f93ba67b26864f
ADOwen <92840021+ADOwen@users.noreply.github.com>
2025-04-02 18:51:05 -0500

    change moduleResolution to node from bundler

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    assetsDir: 'assets',
-    outDir: 'dist',
+  css: {
+    postcss: './postcss.config.js',
   },
-  // We're using postcss.config.js instead of inline config
 })
