904c28cece6669f058991e5360a1ae72bdc12dec
ADOwen <92840021+ADOwen@users.noreply.github.com>
2025-04-02 19:20:24 -0500

    changes to commonJs modules and add postinstall hook

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: './postcss.config.js',
+  build: {
+    assetsDir: 'assets',
+    outDir: 'dist',
   },
+  css: {
+    // Ensure this file gets included in the build
+    preprocessorOptions: {
+      css: {
+        additionalData: `@import "./src/tailwind.css";`
+      }
+    }
+  }
 })
